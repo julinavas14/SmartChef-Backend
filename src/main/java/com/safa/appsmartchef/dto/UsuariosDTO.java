9f2d5abe8e713e9d4dc0b5b32b1d6aa3fd36b003
julinavas14 <jnavasmedina@safareyes.es>
2026-01-21 12:57:09 +0100

    -

--- src/main/java/com/safa/appsmartchef/dto/UsuariosDTO.java
+++ src/main/java/com/safa/appsmartchef/dto/UsuariosDTO.java
@@ -1,10 +1,10 @@
 package com.safa.appsmartchef.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UsuariosDTO {
     private Integer id_usuario;
     private String nombreUsuario;
--- src/test/java/com/safa/appsmartchef/servicios/UsuarioServiceIntegrationTest.java
+++ src/test/java/com/safa/appsmartchef/servicios/UsuarioServiceIntegrationTest.java
@@ -1,8 +1,32 @@
 package com.safa.appsmartchef.servicios;
 
+import com.safa.appsmartchef.conversores.UsuariosMapper;
+import com.safa.appsmartchef.repositorio.UsuariosRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class UsuarioServiceIntegrationTest {
-    
+
+    @InjectMocks
+    private UsuariosService usuariosService;
+
+    @Mock
+    private UsuariosRepository usuariosRepository;
+
+    @Mock
+    private UsuariosMapper usuariosMapper;
+
+    @Test
+    @DisplayName("Servicio 1 - Integración")
+    void buscarPorIdIntegration(){
+        //GIVEN
+
+        //THEN
+
+        //WHEN
+    }
 }
